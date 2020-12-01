369c96c6dfe0c103936e0b26801aea724cd098a3
Robert Aspirot <raspirot@genetec.com>
2020-11-30 23:16:35 -0500

    added checkout page , checkout item component and updated cart to use selectors from reselect

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import store from './redux/store';
 
-import './index.css';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
